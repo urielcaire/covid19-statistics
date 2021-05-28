9c7d4706c335ba22bed637133b88d8c060fff951
urielcaire <uriel.caire@gmail.com>
2021-05-28 00:15:04 -0300

    Analyse full brazil dataset

--- covid19_brazil.r
+++ covid19_brazil.r
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(visdat)
+library(naniar)
 require(data.table)
 home <- getwd()
 
@@ -11,7 +14,25 @@ covid19_brazil <- covid19[covid19$location == "Brazil", c('location', 'date',
                                                           'total_deaths', 'new_deaths',
                                                           'total_vaccinations',
                                                           'new_vaccinations')]
-#View(covid19_brazil)
+# Apply NA if there are null values
+covid19_brazil <- covid19_brazil %>%
+                  na_if((""))
+
+# Analyzing full dataset
+#check data types
+vis_guess(covid19_brazil)
+#proportion of missing values
+vis_miss(covid19_brazil)
+#missing values graph
+gg_miss_var(covid19_brazil)
+#check NA values intersections
+gg_miss_upset(covid19_brazil)
+#check correlation bewtween all numeric columns
+vis_cor(covid19_brazil[,c('total_cases', 'new_cases', 'total_deaths', 'new_deaths',
+                          'total_vaccinations','new_vaccinations')])
+#plot correlation between new_cases,new_deaths and new_vaccinations
+vis_cor(covid19_brazil[,c('new_cases','new_deaths','new_vaccinations')])
+
 # Saving the dataframe as a .csv file
 write.table(covid19_brazil, paste(home, '/data/datasets/covid-brazil-2021-05-22.csv', sep=''),
             sep = ',', row.names = FALSE, quote=FALSE)
