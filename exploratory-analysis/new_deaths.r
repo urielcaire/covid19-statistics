65260b4337cf50774b5412cc32aec99caad3e76f
urielcaire <uriel.caire@gmail.com>
2021-06-17 21:53:54 -0300

    Preliminary (exploratory) analysis

--- exploratory-analysis/new_deaths.r
+++ exploratory-analysis/new_deaths.r
@@ -11,98 +11,70 @@ covid19 <- read.csv(covid19)
 
 # Sort the data by date
 covid19 <- covid19[order(as.numeric(as.Date(covid19$date, format = "%Y-%m-%d"))),]
+# Getting first date of a 'new_death' report
+day_zero <- first(covid19[covid19$new_deaths>=1,'date'])
+day_zero
+# Filtering data by day_zero
+covid19 <- covid19[covid19$date>=as.Date(day_zero),]
 
-
-# Getting first date of reports
-day_zero <- first(covid19[, 'date'])
 # Getting the initial week
 start_week <- as.numeric(strftime(as.Date(day_zero), format = '%U'))
 start_week
 # Getting initial day position in week range (1:7)
 start_day <- as.numeric(strftime(as.Date(day_zero), format = '%w')) + 1
 start_day
-
-# Transforms data into daily frequency TimeSeries with a weekly period
+# Transforms data into daily TimeSeries with a weekly frequency
+#https://otexts.com/fpp2/tspatterns.html
 new_deaths <- ts(covid19[,c('new_deaths')], start = c(start_week, start_day),
                  frequency = 7)
+new_deaths
 
 # Print some infos about new_deaths
 summary(new_deaths)
-# Plot it
-autoplot(new_deaths)
+# Plot it (we can observer a seasonality and "changing direction")
+autoplot(new_deaths) + ggtitle("Óbitos por COVID-19 no Brasil (2020-2021)") +
+  theme(plot.title = element_text(hjust = 0.5)) +  xlab("Semana") +
+  ylab("Número de Óbitos") # plot just it
 # Plot a histogram
 hist(new_deaths)
 # Plot a box plot
 boxplot(new_deaths)
+
 # Plot season
-seasonplot(new_deaths)
+#it shows a frequent jump on sundays to 2020 and 2021, and on weednesdays to 2021
+#number of deaths increases around week 58ª
+#https://otexts.com/fpp2/seasonal-plots.html
+ggseasonplot(new_deaths, season.labels = c("Dom","Seg","Ter","Qua","Qui","Sex","Sab"),
+             polar=TRUE) +
+  ylab("Número de Óbitos") +
+  xlab("Dia da Semana") +
+  ggtitle("Gráfico de sasonalidade: óbitos por Covid19") +
+  scale_color_discrete(name= "Semana (2020-2021)", labels=paste0(11:72,"ª"))
 
-# Residual analysis
-res_model <- auto.arima(tsclean(new_deaths))
-autoplot(res_model$residuals)
-hist(res_model$residuals)
-# Check residuals: seems to be not correlated, p-value = 0.007076
-# correlation table
-checkresiduals(res_model)
-# Check distribution: not normal, p-value < 2.2e-16
-# https://rpubs.com/paternogbc/46768
-shapiro.test(res_model$residuals)
-# Check stationarity: not stationarity, p-value = 0.9216
-adf_test <- adf.test(new_deaths,alternative = 'stationary')
-print(adf_test)
+# Seasonal subseries plots
+#The horizontal lines indicate the means for each day
+#https://otexts.com/fpp2/seasonal-subseries-plots.html
+ggsubseriesplot(new_deaths, labels = c("Dom","Seg","Ter","Qua","Qui","Sex","Sab")) +
+  ylab("Número de Óbitos") +
+  xlab("Dia da Semana") +
+  ggtitle("Gráfico de subséries sazonais: óbitos por Covid19")
 
-# Moving Averages: removes outliers and smooth graph
-plot(new_deaths)
-lines(ma(new_deaths, order=5), col='red')
-# tsclean: it was better for outliers
-plot(new_deaths)
-lines(tsclean(new_deaths), col='green')
-new_deaths <- tsclean(new_deaths)
+# Plot lag plot (plotar o valor de um dado em relação ao seu antecessor)
+#The relationship is strongly positive at lags 7 and 14,
+#reflecting seasonality in the data
+#https://otexts.com/fpp2/lag-plots.html
+gglagplot(new_deaths, 16, do.lines = FALSE)
 
-# Check ndiffs
-ndiffs(new_deaths, test = 'kpss')
+# Plot correlogram
+#autocorrelation larger for lags multiples of 7, indicating seasonality
+#there is a decrease in the ACF as the lags increase, indicating a trend
+#https://otexts.com/fpp2/autocorrelation.html
+#https://rpubs.com/hudsonchavs/fac_facp#:~:text=FUN%C3%87%C3%83O%20DE%20AUTOCORRELA%C3%87%C3%83O&text=onde%20Var(r,rt%20%C3%A9%20fracamente%20estacion%C3%A1rio.
+ggAcf(new_deaths)
 
-# Apply diffs until find best parameter
-alpha <- 0.05
-best_p_value <- 2
-best_param_diff <- 1
-for(i in 1:431){
-  diff_new_deaths <- diff(new_deaths, i)
-  diff_stat_test <- Box.test(diff_new_deaths, type = 'Ljung-Box')
-  # absolute
-  if (abs(diff_stat_test$p.value - alpha) < abs(best_p_value - alpha)){
-    best_p_value <- diff_stat_test$p.value
-    best_param_diff <- i
-  }
-}
-best_p_value
-best_param_diff
+# "Time series that show no autocorrelation are called white noise."
+#so it is not an "white noise"
 
-diff_new_deaths <- diff(new_deaths, best_param_diff)
-diff_stat_test <- Box.test(diff_new_deaths, type = 'Ljung-Box')
-autoplot(diff_new_deaths)
-
-# Compare original and diff new_death ts
-split.screen(figs = c(2,1))
-screen(1)
-plot(new_deaths, main='new_deaths')
-screen(2)
-plot(diff_new_deaths, main='diff_new_deaths')
-
-# Residual analysis of diff_new_deaths
-res_model <- auto.arima(diff_new_deaths)
-autoplot(res_model$residuals)
-hist(res_model$residuals)
-# Check residuals: has a weak correlation,p-value = 0.3915
-checkresiduals(res_model)
-# Check distribution: it is not a normal distribution, p-value = 0.02332
-shapiro.test(res_model$residuals)
-# Check stationarity: seems stationarity, p-value = 0.08105
-adf_test <- adf.test(diff_new_deaths,alternative = 'stationary')
-print(adf_test)
-
-# Apply stlf decomposition
-stlf_result <- stlf(diff_new_deaths, h=7)
-autoplot(stlf_result)
-seasonplot(diff_new_deaths)
-
+# Plot TS seasonality infos(data, seasonal, trend, remainder)
+#https://otexts.com/fpp2/components.html
+plot(stl(new_deaths, "periodic"))
